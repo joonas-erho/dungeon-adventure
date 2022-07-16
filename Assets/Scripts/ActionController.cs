bf9d2d0700962fb7c9a21d5f82e27c798360a433
Joonas Erho <joonas.erho@gmail.com>
2022-07-16 20:57:32 +0300

    Actions can now be added to queue

--- Assets/Scripts/ActionController.cs
+++ Assets/Scripts/ActionController.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class ActionController : MonoBehaviour
 {
-    public ActionScriptableObject obj;
+    // GameController that handles the whole scene.
+    // This reference is added as actions are generated.
+    private GameController gameController;
+
+    // The scriptable object that dictates the look and functionality
+    // of this action.
+    private ActionScriptableObject action;
+
+    // Sprite renderers of the action base and the icon, respectively.
     public SpriteRenderer sr;
     public SpriteRenderer iconSr;
 
+    // On generation, set the look to match that of the scriptable object's.
     void Start() {
-        sr.color = obj.color;
-        iconSr.sprite = obj.icon;
+        sr.color = action.color;
+        iconSr.sprite = action.icon;
+    }
+
+    public void SetValues(ActionScriptableObject action, GameController gc) {
+        this.action = action;
+        this.gameController = gc;
     }
 
     void OnMouseDown() {
-        Debug.Log(obj.name);
+        gameController.AddActionToQueue(action);
     }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    // Readonly values.
+    private readonly float actionButtonGap = 0.8f;
+    private readonly int amountOfActionsInRow = 11;
+
     // Prefab for the Action boxes.
     public GameObject actionPrefab;
 
@@ -15,6 +19,9 @@ public class GameController : MonoBehaviour
 
     // List of all actions. (Used mainly editor-side)
     public List<ActionScriptableObject> actions = new();
+
+    // List of actions in the queue.
+    private List<ActionController> queuedActions = new();
     
 
     void Start() {
@@ -22,23 +29,36 @@ public class GameController : MonoBehaviour
     }
 
     /// <summary>
-    /// Generates available actions and places them as children to the "Action Store"
-    /// gameobject.
+    /// Generates available actions and places them as children to the "Action Store" gameobject.
     /// </summary>
     /// <param name="list">List of actions as premade scriptable objects. (See Assets/Objects-folder.)</param>
     void GenerateAvailableActions(List<ActionScriptableObject> list) {
         for (int i = 0; i < list.Count; i++) {
-            // Create game object and add as child to action store in correct position.
-            GameObject go = Instantiate(actionPrefab);
-            go.transform.SetParent(actionStore.transform);
-            go.transform.localPosition = new Vector3(i * 0.85f, 0, 0);
-
-            // Set correct name (visible in editor only).
-            go.name = "Action (" + list[i].actionName + ")";
-            
-            // Set the object of the created prefab as the one given in the list.
-            // See ActionController for more information.
-            go.GetComponent<ActionController>().obj = list[i];
+            CreateAction(list[i], actionStore, new Vector2((i % amountOfActionsInRow) * actionButtonGap, (i / amountOfActionsInRow) * -actionButtonGap));
         }
+    }
+
+    private GameObject CreateAction(ActionScriptableObject action, GameObject parent, Vector2 position) {
+        // Create game object and add as child to action store in correct position.
+        GameObject go = Instantiate(actionPrefab);
+        go.transform.SetParent(parent.transform);
+        go.transform.localPosition = position;
+
+        // Set correct name (visible in editor only).
+        go.name = "Action (" + action.actionName + ")";
+        
+        // Set the object of the created prefab as the one given in the list.
+        // See ActionController for more information.
+        go.GetComponent<ActionController>().SetValues(action, this);
+
+        return go;
+    }
+
+    public void AddActionToQueue(ActionScriptableObject action) {
+        GameObject go =
+            CreateAction(action,
+                         actionQueue,
+                         new Vector2((queuedActions.Count % amountOfActionsInRow) * actionButtonGap, (queuedActions.Count / amountOfActionsInRow) * -actionButtonGap));
+        queuedActions.Add(go.GetComponent<ActionController>());
     }
 }
